ac5196d1c5c8d034dc97bc44370a0ce4465b0c8e
MaxHuw <m.huw.graham@gmail.com>
2019-05-25 17:47:17 -0400

    Can add tasks and subtasks.

--- resolvers/mutations.js
+++ resolvers/mutations.js
@@ -1,3 +1,35 @@
 module.exports = {
   
+  addTask: async (parent, args, { db }, info) => {
+    await db.Task.create({
+      title: args.title,
+      description: args.description,
+      status: args.status,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      dueDate: args.dueDate,
+      userId: args.userId,
+      categoryId: args.categoryId
+    }).then( () => {
+      console.log("New Task Created.")
+    }).catch(console.log)
+
+    return true
+  },
+
+  addSubtask: async (parent, args, { db }, info) => {
+    await db.Subtask.create({
+      title: args.title,
+      description: args.description,
+      status: args.status,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      dueDate: args.dueDate,
+      taskId: args.taskId,
+    }).then( () => {
+      console.log("New Task Created.")
+    }).catch(console.log)
+
+    return true
+  }
 };
--- schema/mutations.js
+++ schema/mutations.js
@@ -4,5 +4,6 @@ const { gql } = require("apollo-server-express");
 module.exports = `
   type Mutation {
     addTask(title: String, description: String, status: String, dueDate: Date, userId: Int, categoryId: Int): Boolean
+    addSubtask(title: String, description: String, status: String, dueDate: Date, taskId: Int): Boolean
   }
 `;
